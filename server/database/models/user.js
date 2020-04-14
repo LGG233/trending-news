f923f4885756e336300f5839e16279d903411fa5
Kyle Whitaker <kwhitaker@level20.com>
2020-04-14 15:06:35 -0400

    revert user model reference changes

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Pub = require('./pub').schema;
-const Article = require('./article').schema;
-const Topic = require('./topic').schema;
 
 // define userSchema
 const userSchema = new Schema({
   username: { type: String, unique: true, required: true },
   name: { type: String, unique: false, required: false },
   email: { type: String, unique: true, required: false },
-  publications: [Pub],
-  savedArticles: [Article],
-  topics: [Topic],
+  publications: [
+    {
+      id: { type: Number, unique: true, sparse: true, required: true },
+      name: { type: String, unique: false, required: false },
+      twitterHandle: { type: String, unique: true, sparse: true, required: true },
+    },
+  ],
+  savedArticles: [
+    {
+      id: { type: Number, unique: true, sparse: true, required: true },
+      title: { type: String, unique: false, required: false },
+      link: { type: String, unique: true, sparse: true, required: true },
+      tags: { type: Array, unique: false, required: false },
+    },
+  ],
+  topics: [
+    {
+      id: { type: Number, unique: true, sparse: true, required: true },
+      name: { type: String, unique: false, required: false },
+      searchTerm: { type: Array, unique: false, required: false },
+    },
+  ],
 });
 
 const User = mongoose.model('User', userSchema);
