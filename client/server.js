348b4575ba9bc0ba46b8901ff4097bc024a39f2f
Kyle Whitaker <kwhitaker@level20.com>
2020-04-15 13:43:25 -0400

    redirect http to https on Heroku (#11)

--- client/server.js
+++ client/server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+// redirect http to https on Heroku
+app.use(function (req, res, next) {
+  if (process.env.NODE_ENV !== 'production') return next();
+  if (req.headers['x-forwarded-proto'] === 'https') return next();
+  return res.redirect(301, `https://${req.hostname}${req.originalUrl}`);
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get(/.*/, function (req, res) {
